b1db12832bdd765e8815e1ba5e1888c832ad6f6b
Mahmoud Emad <110336040+Shoeib-1@users.noreply.github.com>
2023-10-30 01:41:38 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,131 @@
+// var degree = parseFloat(prompt("Enter your degree", ""));
+
+// var result = degree > 50 ? "sucess":"fail";
+
+
+// document.write(result);
+
+// var degree = parseInt(prompt("Enter your num", ""));
+// if (degree<50) {
+//     document.write( "<h1 style='color:red'>Fail</h1>");
+// }
+// else if( degree>=50 && degree<70){
+//     document.write("<h1 style='color:cyan'>passed</h1>");
+// }else if( degree>=70 && degree<80){
+//     document.write("<h1 style='color:green'>good</h1>");
+// }else{
+//     document.write("<h1 style='color:green'>Excellent</h1>");
+// }
+"use strict";
+// var num1 = parseInt(prompt("Enter your frist num", ""));
+// var num2 = parseInt(prompt("Enter your sec num", ""));
+// var process = prompt("Enter your process", "");
+// let result;
+// switch (num1, num2, process) {
+//     case process = "*":
+//         result = num1 + "*" + num2 + "=" + num1 * num2
+//         document.write(result);
+//         break;
+//     case process = "+":
+//         result = num1 + num2
+//         document.write(result);
+//         break;
+//     case process = "-":
+//         result = num1 - num2
+//         document.write(result);
+//         break;
+//     default:
+//         document.write("undefined operation");
+
+// }
+// document.getElementsByTagName("body")[0].style.background = "yellow";
+// for (let num = 10; num >= 0; num--) {
+//     if (num % 2 == 0) {
+//         document.write("<h1 style='color:green'>" + num + "Is Even</h1>");
+//     } else {
+//         document.write("<h1 style='color:red'>" + num + "Is odd</h1>");
+//     }
+// }
+
+// var rkm = 0;
+// for (let i = 1; i <= 10; i++) {
+//     var mm= parseInt(prompt("Enter number"));
+//     if (mm % 2 == 0) {
+//         rkm = rkm + mm
+//     }
+// }
+// document.write("the rkm = ", rkm);
+
+
+
+function tot(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// const sec = document.getElementById("header1");
+// sec.style.backgroundColor = "cadetblue";
+// sec.style.marginBottom = "20px";
+// sec.style.width = "50%";
+
+// const fnew = document.createElement("h1");
+
+// sec.prepend(fnew)
+
+// fnew.classList.add("mm", "bb");
+// //  fnew.classList.remove("mm")
+// fnew.innerText = "مرحبا"
+
+
+// const head = document.querySelector(".header2");
+// head.style.backgroundColor = "red";
+// head.style.padding = "30px"
+// head.style.width = "40%";
+// // head.remove();
+
+// const ffnew = document.createElement("p");
+
+// head.prepend(ffnew);
+// ffnew.classList.add("prince");
+// ffnew.innerText = "hello man"
+// ffnew.setAttribute("id", "myId");
+// ffnew.setAttribute("style", "background-color:green ;")
+// ffnew.style.backgroundColor = "Yellow";
+// ffnew.style.width = "60%";
+
+// const body = document.getElementsByTagName("body")[0];
+
+
+// function myFunc() {
+//     document.getElementsByClassName("para")[0].setAttribute("style", "color:Yellow");
+//     ffnew.style.backgroundColor = "#2d2d2d";
+//     head.style.backgroundColor = "cyan";
+//     sec.style.backgroundColor = "#cdcdcd";
+//     fnew.style.color = "red"
+//     body.style.backgroundColor = "#a63d40";
+// }
+// // setTimeout(myFunc,10000);
+
+
+// x=5;
+// var x =10;
+// document.write(x);
+
+
+// for (let i = 0; i < 3; i++) {
+//     setTimeout(() => {
+//         console.log(i++);
+//     }, 1000);
+    
+// }
+
